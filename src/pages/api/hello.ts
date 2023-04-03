7a448aa50394d9fd8ec1469e18a93c3a03f54f7e
Kuba Florczuk <me@kuba.rocks>
2023-04-03 19:17:00 +0200

    fix hello endpoint

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,9 +1,10 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export const config = {
   runtime: 'edge',
 }
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default function hello(req: NextApiRequest, res: NextApiResponse) {
   const { name = "World" } = req.query;
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   res.status(200).json({ name: "John Doe", greeting: `Hello ${name}!` });
 }
