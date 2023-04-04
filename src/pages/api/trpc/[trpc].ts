e9377f85904f1c99bb319c09db958991bb325628
Kuba Florczuk <me@kuba.rocks>
2023-04-04 17:40:14 +0200

    fix build

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,29 +1,17 @@
-// import { createNextApiHandler } from "@trpc/server/adapters/next";
+import { createNextApiHandler } from "@trpc/server/adapters/next";
 
-// import { createTRPCContext } from "@app/server/api/trpc";
-// import { appRouter } from "@app/server/api/root";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { createTRPCContext } from "@app/server/api/trpc";
+import { appRouter } from "@app/server/api/root";
 
 export const config = {
   runtime: "edge",
 };
 
 // export API handler
-// export default createNextApiHandler({
-//   router: appRouter,
-//   createContext: createTRPCContext,
-//   onError: ({ path, error }) => {
-//     console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
-//   },
-// });
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // console.log(appRouter);
-  // console.log(createTRPCContext);
-  // console.log(createNextApiHandler);
-
-  res.status(200).json({
-    name: process.env.NEXT_RUNTIME,
-    config,
-  });
-}
+export default createNextApiHandler({
+  router: appRouter,
+  createContext: createTRPCContext,
+  onError: ({ path, error }) => {
+    console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
+  },
+});
