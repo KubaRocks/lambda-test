0b7a6b6b187166846f60bd5afcca3fe9973e34f5
Kuba Florczuk <me@kuba.rocks>
2023-04-04 17:05:27 +0200

    minimal tRPC endpoint to make it work

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,23 +1,29 @@
-import { createNextApiHandler } from "@trpc/server/adapters/next";
+// import { createNextApiHandler } from "@trpc/server/adapters/next";
 
-import { env } from "@app/env.mjs";
-import { createTRPCContext } from "@app/server/api/trpc";
-import { appRouter } from "@app/server/api/root";
+// import { createTRPCContext } from "@app/server/api/trpc";
+// import { appRouter } from "@app/server/api/root";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export const config = {
   runtime: "edge",
 };
 
 // export API handler
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: createTRPCContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
-          );
-        }
-      : undefined,
-});
+// export default createNextApiHandler({
+//   router: appRouter,
+//   createContext: createTRPCContext,
+//   onError: ({ path, error }) => {
+//     console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
+//   },
+// });
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  // console.log(appRouter);
+  // console.log(createTRPCContext);
+  // console.log(createNextApiHandler);
+
+  res.status(200).json({
+    name: process.env.NEXT_RUNTIME,
+    config,
+  });
+}
