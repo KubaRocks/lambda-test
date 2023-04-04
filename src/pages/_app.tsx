2808e32c4d99afa66b398cb5513c432f526d9e0f
Kuba Florczuk <me@kuba.rocks>
2023-04-04 08:41:45 +0200

    add logrocket

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { type AppType } from "next/app";
+import LogRocket from "logrocket";
 
 import { api } from "@app/utils/api";
 
 import "@app/styles/globals.css";
 
+LogRocket.init("hrfn4w/lamba-test");
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
